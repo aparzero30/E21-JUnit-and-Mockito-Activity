7e40ba82ad94c337c7fc21ae5964c6b968d3ae08
Jimuel Cinco <jimuel.cinco@novare.com.hk>
2023-01-25 14:35:43 +0800

    first commit

---
+++ src/test/java/AdvanceMathTester.java
@@ -0,0 +1,161 @@
+import junit.framework.Assert;
+import org.example.AdvanceMath;
+import org.example.BasicMath;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdvanceMathTester {
+
+    @InjectMocks
+    AdvanceMath advanceMath = new AdvanceMath();
+
+    @Mock
+    BasicMath basicMath;
+
+
+
+   //TEST1
+    @Test
+    public void  testMultiplyDifferenceBy5(){
+
+        when(basicMath.subtract(80.0,20.0)).thenReturn((60.0));
+        Assert.assertEquals(advanceMath.multiplyDifferenceBy5(80.0,20.0), 300.0, 0);
+        verify(basicMath).subtract(80.0,20.0);
+
+    }
+
+
+
+   //TEST2
+
+    @Test
+    public void  testMultiplySumBy5(){
+
+        when(basicMath.add(75.0,25.0)).thenReturn((100.0));
+        Assert.assertEquals(advanceMath.multiplySumBy5(75.0,25.0), 500.0, 0);
+        verify(basicMath).add(75.0,25.0);
+
+    }
+
+
+    //TEST3
+    @Test
+    public void  testSquareOfSum(){
+
+        when(basicMath.add(20.0,20.0)).thenReturn((40.0));
+        Assert.assertEquals(advanceMath.squareOfSum(20.0,20.0), 1600.0, 0);
+        verify(basicMath).add(20.0,20.0);
+
+    }
+
+
+
+    //TEST4
+    @Test
+    public void  testGetPercentage(){
+
+        when(basicMath.divide(50.0,100.0)).thenReturn((0.5));
+        Assert.assertEquals(advanceMath.getPercentage(50.0,100.0), 50.0, 0);
+       verify(basicMath).divide(50.0,100.0);
+
+         reset(basicMath);
+
+        when(basicMath.divide(50.0,100.0)).thenReturn((0.5));
+        Assert.assertEquals(advanceMath.getPercentage(50.0,100.0), 50.0, 0);
+        verify(basicMath).divide(50.0,100.0);
+    }
+
+
+
+
+    //TEST5
+    @Test
+    public void  testMultiplyDifferenceBy5BDDFormat(){
+
+
+        //given
+        given(basicMath.subtract(80.0,20.0)).willReturn((60.0));
+
+        //when
+        double result = advanceMath.multiplyDifferenceBy5(80.0,20.0);
+        Assert.assertEquals(result, 300.0, 0);
+        verify(basicMath).subtract(80.0,20.0);
+
+
+    }
+
+
+    //TEST6
+    @Test
+    public void  testMultiplySumBy5BDDFormat(){
+
+        //given
+        given(basicMath.add(75.0,25.0)).willReturn((100.0));
+
+        //when
+        double result = advanceMath.multiplySumBy5(75.0,25.0);
+
+
+        //then
+        Assert.assertEquals(result, 500.0, 0);
+        verify(basicMath).add(75.0,25.0);
+
+    }
+
+
+    //TEST7
+    @Test
+    public void  testSquareOfSumBDDFormat(){
+
+        //given
+        given(basicMath.add(20.0,20.0)).willReturn((40.0));
+
+        //when
+        double result = advanceMath.squareOfSum(20.0,20.0);
+
+        //then
+        Assert.assertEquals(result, 1600.0, 0);
+        verify(basicMath).add(20.0,20.0);
+
+    }
+
+
+
+    //TEST8
+    @Test
+    public void  testGetPercentageBDDFormat(){
+
+        //given
+        given(basicMath.divide(50.0,100.0)).willReturn((0.5));
+
+        //when
+        double result = advanceMath.getPercentage(50.0,100.0);
+
+       //then
+        Assert.assertEquals(result, 50.0, 0);
+        verify(basicMath).divide(50.0,100.0);
+
+        reset(basicMath);
+
+        //given
+        given(basicMath.divide(50.0,100.0)).willReturn((0.5));
+
+        //when
+        result = advanceMath.getPercentage(50.0,100.0);
+
+        //then
+        Assert.assertEquals(result, 50.0, 0);
+        verify(basicMath).divide(50.0,100.0);
+
+
+    }
+
+
+}
